1f22450e2dbfda28161b5bd37f46cdccd3ed6307
Skarginson <102185297+Skarginson@users.noreply.github.com>
2024-05-01 09:47:24 +0200

    done

---
+++ src/components/TimeStamp.jsx
@@ -0,0 +1,6 @@
+// src/components/Timestamp.jsx
+function Timestamp({ time }) {
+  return <span className="timestamp">{time}</span>;
+}
+
+export default Timestamp;
--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,36 +1,25 @@
-function Tweet( {tweet: {user, timestamp, message}}) {
+// function Tweet( {tweet: {user, timestamp, message}}) {
+
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./TimeStamp";
+import Message from "./Message";
+import Actions from "./Actions";
+
+function Tweet({ tweet: { user, timestamp, message } }) {
   return (
     <div className="tweet">
-      <img
-        src={user.image}
-        className="profile"
-        alt="profile"
-      />
+      <ProfileImage image={user.image} />
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">{user.name}</span>
-            <span className="handle">{user.handle}</span>
-          </span>
-
-          <span className="timestamp">{timestamp}</span>
+          <User name={user.name} handle={user.handle} />
+          <Timestamp time={timestamp} />
         </div>
 
-        <p className="message">
-        {message}
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment" data-testid="comment-icon"></i>
-          <i className="fas fa-retweet" data-testid="retweet-icon"></i>
-          <i className="far fa-heart" data-testid="heart-icon"></i>
-          <i className="fas fa-share" data-testid="share-icon"></i>
-        </div>
+        <Message message={message} />
+        <Actions />
       </div>
-
-      <i className="fas fa-ellipsis-h"></i>
     </div>
   );
 }
